f5982315c7b14e2ae9a496a463e5dddd70ef6be6
Coder <jjvalela0203@gmail.com>
2026-09-24 10:11:05 -0500

    Dockerized finished

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
-from sqlmodel import create_engine, Session
+from sqlalchemy import create_engine
+from sqlmodel import Session
 import os
 
-Database_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1140918021@localhost:5432/Productos_DB")
+Database_URL = os.getenv("DATABASE_URL")
+
+if not Database_URL:
+    raise ValueError("Falta la variable de entorno DATABASE_URL")
 
 engine = create_engine(Database_URL, echo=True)
 
